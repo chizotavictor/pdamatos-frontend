d643944b6b21fdf3e1bec2a3acb65cf89eefa084
Ebuka Chizota <chizotavictor@gmail.com>
2021-07-28 13:10:28 +0100

    second release

--- src/general-service.js
+++ src/general-service.js
@@ -2,8 +2,8 @@ import http from "./http-common";
 
 class Service {
   constructor() {
-    this.baseUrl = "http://localhost/pdamatos/index.php/api";
-    // this.baseUrl = "https://pdamatos.ltd/v1/public/index.php/api"; //"http://localhost/pdamatos/api"
+    // this.baseUrl = "http://localhost/pdamatos/index.php/api";
+    this.baseUrl = "https://pdamatos.ltd/v1/public/index.php/api"; //"http://localhost/pdamatos/api"
     this.authorization = "?token=" + localStorage.getItem("_key");
 
     const token = localStorage.getItem("_key");
